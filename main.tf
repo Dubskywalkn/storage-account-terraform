8cb61f77c41004d79876814009eed44adf42bfd1
Dubskywalkn <jaydentea2@gmail.com>
2025-05-01 21:31:39 -0500

    yooo

--- main.tf
+++ main.tf
@@ -5,8 +5,12 @@ resource "azurerm_resource_group" "RG" {
 
 resource "azurerm_storage_account" "SA" {
   name                     = var.storage_name
-  resource_group_name      = var.resource_group
+  resource_group_name      = azurerm_resource_group.RG.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  depends_on = [azurerm_resource_group.RG]
 }
+
+
